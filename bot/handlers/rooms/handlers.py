f7a1dcad46263f0a92fa259f9da3ca6e5a1f3409
semyon danshin <semen.danshin@gmail.com>
2023-11-03 22:09:43 +0300

    Fix thing

--- bot/handlers/rooms/handlers.py
+++ bot/handlers/rooms/handlers.py
@@ -1099,6 +1099,7 @@ async def process_agent_commission(update: Update, context: ContextTypes.DEFAULT
 
     advertisement = await advertisement_service.get_advertisement(session, data.get('ad_id'))
 
+    await session.refresh(advertisement, attribute_names=['room'])
     await session.refresh(advertisement.room, attribute_names=["rooms_info"])
 
     advertisement = AdvertisementResponse.model_validate(advertisement)
